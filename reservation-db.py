302cdfaa23411401215ccb11cf993f41c081d018
gautamsihag <gautamsihag297@gmail.com>
2016-03-12 22:46:43 -0500

    Update reservation-db.py

--- reservation-db.py
+++ reservation-db.py
@@ -1,11 +1,18 @@
+# using the functions from util.py to
+# calculate entropy and rate based on the state of the Redis db. It takes no
+# input, and emits a JSON string with the entropy and rate to stdout. These
+# messages are monitored by check-detect.py to, find outliers.
 import json
 from sys import stdout
 from time import sleep
 import util
-
+#Repeat the entropy and rate calculation
 while True:
+    # using the util function to calculate entropy and rate
     entropy = util.entropy()
     rate = util.rate()
+    # the entropy and the rate over to stdout
     print (json.dumps({'entropy':entropy,'rate':rate}))
     stdout.flush()
+    # sleep for a second: to ensure to get a smoother function for the entropy values and rate.
     sleep(1)
